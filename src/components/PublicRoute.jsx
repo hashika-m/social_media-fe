4edcd03b910beec2330b237baeaf64731a1444eb
hashika-m <hashika0302@gmail.com>
2026-03-23 11:48:32 +0530

    change

--- src/components/PublicRoute.jsx
+++ src/components/PublicRoute.jsx
@@ -3,11 +3,11 @@
 
 // const PublicRoute = ({ children }) => {
 //   const { userData } = useSelector((state) => state.user)
-  
+
 //   if (userData) {
 //     return <Navigate to="/home" replace />
 //   }
-  
+
 //   return children
 // }
 
@@ -21,7 +21,11 @@ const PublicRoute = ({ children }) => {
   const { userData, authLoading } = useSelector((state) => state.user)
 
   if (authLoading) {
-    return <div>Loading...</div>
+    return (
+      <div className="w-full min-h-screen flex items-center justify-center bg-gray-100">
+        <div className="animate-spin h-12 w-12 rounded-full border-b-2 border-amber-600"></div>
+      </div>
+    )
   }
 
   if (userData) {
